c657f3a52447d97c7419cf491091289fcbea38e6
Vikash Sharma <expertvikash@gmail.com>
2024-10-10 23:46:10 +0530

    commit

--- week10.2/prisma/dist/index.js
+++ week10.2/prisma/dist/index.js
@@ -1,2 +1,41 @@
 "use strict";
-console.log("hello world");
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const client_1 = require("@prisma/client");
+const prisma = new client_1.PrismaClient();
+function insertUser(username, password, firstname, lastname) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const res = yield prisma.users.create({
+            data: {
+                username,
+                password,
+                firstname,
+                lastname
+            }
+        });
+        console.log(res);
+    });
+}
+// insertUser("vikash.sharma_mca24@gla.ac.in","vikash.12","vikash","sharma");
+function getUser(fname) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const user = yield prisma.users.findFirst({
+            select: {
+                username: true
+            },
+            where: {
+                firstname: fname
+            }
+        });
+        console.log(user);
+    });
+}
+getUser("vikash");
--- week10.2/prisma/src/index.ts
+++ week10.2/prisma/src/index.ts
@@ -1,1 +1,33 @@
-console.log("hello world")
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+
+
+async function insertUser(username:string,password:string,firstname:string,lastname:string) {
+    const res = await prisma.users.create({
+        data:{
+            username,
+            password,
+            firstname,
+            lastname
+        }
+    })
+
+    console.log(res);
+}
+
+// insertUser("vikash.sharma_mca24@gla.ac.in","vikash.12","vikash","sharma");
+
+async function getUser(fname: string) {
+    const user = await prisma.users.findFirst({
+        select:{
+           
+            username:true
+        },
+      where: {
+          firstname: fname
+      }
+    })
+    console.log(user);
+  }
+  
+  getUser("vikash");
